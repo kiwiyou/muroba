cd34ba0f4bc3092b756b07f00be71a535c160dd3
kiwiyou <kiwiyou.dev@gmail.com>
2021-02-12 12:21:36 +0900

    Rename example variable to dynamic

---
+++ examples/dynamic.rs
@@ -0,0 +1,18 @@
+use muroba::{
+    style::{DefaultStyle, Style},
+    Interactive, Promptable,
+};
+
+fn main() {
+    let choice = DefaultStyle::dynamic_select("Type Something", "Waiting for Result...", |input| {
+        input.split_ascii_whitespace().map(str::to_string).collect()
+    })
+    .with_prompt("Choose a word within what you typed")
+    .interact()
+    .unwrap();
+    if let Some(choice) = choice {
+        println!("You selected {}!", choice);
+    } else {
+        println!("You did not select anything.");
+    }
+}
